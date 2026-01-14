c02657c25ba087270acbe69b0dd73285e4908179
David <ddave17@gmail.com>
2026-01-13 23:26:55 -0500

    Add AnyAuthorizePermissionAttribute for flexible access authorization

--- src/DavidHome.Optimizely.VirtualText/Controllers/DefaultController.cs
+++ src/DavidHome.Optimizely.VirtualText/Controllers/DefaultController.cs
@@ -1,11 +1,10 @@
 ﻿using DavidHome.Optimizely.VirtualText.Plugin;
 using DavidHome.Optimizely.VirtualText.Routing;
-using EPiServer.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DavidHome.Optimizely.VirtualText.Controllers;
 
-[AuthorizePermission(PluginPermissions.GroupName, PluginPermissions.ViewPermissionsName)]
+[AnyAuthorizePermission(PluginPermissions.GroupName, PluginPermissions.ViewPermissionsName, PluginPermissions.EditPermissionsName)]
 [ModuleRoute]
 public class DefaultController : Controller
 {
---
+++ src/DavidHome.Optimizely.VirtualText/Plugin/AnyAuthorizePermissionAttribute.cs
@@ -0,0 +1,35 @@
+﻿using EPiServer.Security;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DavidHome.Optimizely.VirtualText.Plugin;
+
+/// <summary>
+/// Allows access authorization to any of the specified name under a certain group.
+/// This class is highly inspired from <see cref="EPiServer.Web.Mvc.AuthorizePermissionAttribute"/>.
+/// </summary>
+public class AnyAuthorizePermissionAttribute : ActionFilterAttribute, IAuthorizationFilter
+{
+    private readonly IReadOnlyCollection<PermissionType> _permissions;
+    
+    public AnyAuthorizePermissionAttribute(string groupName, params string[] names)
+    {
+        _permissions = names.Select(name => new PermissionType(groupName, name)).ToArray();
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var permissionService = context.HttpContext.RequestServices.GetRequiredService<PermissionService>();
+        var user = context.HttpContext.User;
+
+        if (_permissions.Any(permission => permissionService.IsPermitted(user, permission)))
+        {
+            return;
+        }
+
+        var userAuthenticated = user.Identity?.IsAuthenticated ?? false;
+        
+        context.Result = userAuthenticated ? new ForbidResult() : new ChallengeResult();
+    }
+}
